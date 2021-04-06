41da912ca1b91f47dfdf13c6cf2a6ef5f23f1985
DESKTOP-JLFLRBR\DubleFake <eternalsuffering101@gmail.com>
2021-04-06 14:29:40 +0300

    + Prime number validation + Input sequence changed

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,13 +12,27 @@ public class Main {
     private static final Scanner sc = new Scanner(System.in);
 
     private static int p, q, n, z, d = 0, e, i;
+    private static List<Integer> primeNumbers = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009));
 
     public static void prepareVariables() {
+    	
+    	while(true) {
         System.out.println("Enter 1st prime number p");
         p = sc.nextInt();
+        if(primeNumbers.contains(p))
+        	break;
+        else
+        	System.out.println("Your number is not prime or it's greater than 1009");
+        }
+    	
+    	while(true) {
         System.out.println("Enter 2nd prime number q");
         q = sc.nextInt();
-
+        if(primeNumbers.contains(q))
+        	break;
+        else
+        	System.out.println("Your number is not prime or it's greater than 1009");
+    	}
         n = p * q;
         z = (p - 1) * (q - 1);
         System.out.println("the value of z = " + z);
@@ -89,9 +105,9 @@ public class Main {
     }
 
     public static void main(String args[]) {
+        prepareVariables();
         System.out.println("Enter the text to be encrypted and decrypted");
         String msg = sc.nextLine();
-        prepareVariables();
 
         double[] c = encrypt(msg);
         System.out.println("Encrypted message is: " + Arrays.toString(c));
