5496859900ef516ebbde3274d9f30ce3bfd95cc9
DubleFake <eternalsuffering101@gmail.com>
2021-04-07 16:31:11 +0300

    - Fix bug: Algorithm would accep small numbers. - Extra data validation added.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final Scanner sc = new Scanner(System.in);
+	private static final Scanner sc = new Scanner(System.in);
 
     private static int p, q, n, z, d = 0, e, i;
-    private static List<Integer> primeNumbers = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009));
+    private static List<Integer> primeNumbers = new ArrayList<>(Arrays.asList(11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009));
 
     public static void enterPQ() {
     	
@@ -22,16 +22,16 @@ public class Main {
             if(primeNumbers.contains(p))
             	break;
             else
-            	System.out.println("Your number is not prime or it's greater than 1009");
+            	System.out.println("Your number is not prime, it's greater than 1009 or less than 11.");
             }
         	
         	while(true) {
             System.out.println("Enter 2nd prime number q");
             q = sc.nextInt();
-            if(primeNumbers.contains(q))
+            if(primeNumbers.contains(q) && p!=q && p<q)
             	break;
             else
-            	System.out.println("Your number is not prime or it's greater than 1009");
+            	System.out.println("Your number is not prime, it's greater than 1009, less than 11 or equals to the previous number.");
         	}
         	
         	prepareVariables();
@@ -155,7 +155,6 @@ public class Main {
     public static void main(String args[]) {
     	String mode;
     	while(true) {
-    		
     		System.out.println("Enter mode (Encrypt/Decrypt):");
     		mode = sc.nextLine();
     		
@@ -167,9 +166,8 @@ public class Main {
     	String msg, temp[];
     	if(mode.toUpperCase().equals("ENCRYPT")) {
     		
-            
             System.out.println("Enter the text to be encrypted");
-            msg = sc.nextLine();
+            msg = sc.next();
             enterPQ();
 
             double[] c = encrypt(msg);
